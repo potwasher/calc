e4f37f2b8ad19c8f40ac6185a1d5c7ce62af66ef
potwasher <60651405+potwasher@users.noreply.github.com>
2020-04-05 14:00:41 -0700

    Update zeusEvo.cpp

--- zeusEvo.cpp
+++ zeusEvo.cpp
@@ -4,60 +4,73 @@
 #include <iomanip>
 using namespace std;
 
-const int TRIALS = 100000;
-const int TARGET_DMG = 20;
+const int TRIALS = 1000000;
 
 int getzeus();
-bool countZeus(int array[], int size);
+bool countZeus(int array[], int size, int target_dmg);
 
-int main(){
+int main()
+{
     srand(static_cast<unsigned int>(time(nullptr)));
-    
-    cout << "Over " << TARGET_DMG
-         << " damage with Zeus (" << TRIALS << " trials)" <<endl;
-    
-    for(int evo = 1; evo <= 20; ++evo)
+
+    for(int target_dmg = 10; target_dmg <= 20; target_dmg+=2)
     {
-        int zeusCount = 0;
-        for(int trial = 0; trial < TRIALS; ++trial)
+        cout << "Over " << target_dmg
+             << " damage with Zeus (" << TRIALS << " trials)" <<endl;
+
+        for(int evo = 1; evo <= 20; ++evo)
         {
-            int zeusEffect[evo];
-            for(int ele = 0; ele < evo; ++ele){
-                zeusEffect[ele] = getzeus();
+            int zeusCount = 0;
+            for(int trial = 0; trial < TRIALS; ++trial)
+            {
+                int zeusEffect[evo];
+                for(int ele = 0; ele < evo; ++ele)
+                {
+                    zeusEffect[ele] = getzeus();
+                }
+
+                if(countZeus(zeusEffect, evo, target_dmg))
+                {
+                    ++zeusCount;
+                }
             }
-            
-            if(countZeus(zeusEffect, evo)){
-                ++zeusCount;
+            if(zeusCount > 0)
+            {
+                cout << "Evolved " << setw(2) << evo << " times: "
+                     << static_cast<double>(zeusCount)/TRIALS * 100 << '%' << endl;
             }
-        }
-        if(zeusCount > 0){
-            cout << "Evolved " << setw(2) << evo << " times: "
-            << zeusCount << endl;
         }
     }
 }
 
-int getzeus(){
+int getzeus()
+{
     return rand()%5;
 }
 
-bool countZeus(int array[], int size){
+bool countZeus(int array[], int size, int target_dmg)
+{
     int dmgCount = 5;
     bool storm = false;
     for(int index = 0; index < size; ++index)
     {
-        switch(array[index]){
-            case 0: dmgCount += 2;
-                    storm = true;
-                    break;
-            case 1: dmgCount += 3;
-                    break;
-            case 2: ++dmgCount;
-                    break;
-            case 3:
-            default: break;
+        switch(array[index])
+        {
+        case 0:
+            dmgCount += 2;
+            storm = true;
+            break;
+        case 1:
+            dmgCount += 3;
+            break;
+        case 2:
+            ++dmgCount;
+            break;
+        case 3:
+        default:
+            break;
         }
     }
-    
-    return (dmgCount >= TARGET_DMG && storm);
+
+    return (dmgCount >= target_dmg && storm);
 }
